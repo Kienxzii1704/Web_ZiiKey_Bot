0feef203d2a2cc392c1fa090730808e25d08c240
Kienxzii1704 <88337934+Kienxzii1704@users.noreply.github.com>
2022-06-18 14:31:44 +0700

    Zzzz

---
+++ js/team.js
@@ -0,0 +1,32 @@
+/*
+THIS CODE BY LWJerri#3290
+*/
+const boxOwners = document.getElementById("owners_list");
+/*
+you can use this api but if you need to create custome api check:
+ https://github.com/Hadi-Koubeissi/discord-web-api
+*/
+const API = "https://discord-web-api.glitch.me/discord/user/";
+
+const owners = [
+    {
+        "id": "442793591501357056",
+        "post": "Bot Owner",
+        "GHURL": "",
+        "InstaURL": "",
+        "YTURL": ""
+    }
+]
+
+for (let indexOne = 0; indexOne < owners.length; indexOne++) {
+    const elementOwners = owners[indexOne];
+
+    $.getJSON(API + elementOwners.id)
+        .then(output => {
+            if (!output.username || !output.url) {
+                setTimeout(function () {
+                    document.querySelectorAll(".banner img").forEach(imgs => imgs.src = url + "../assets/bot.png");
+                }, 1000);
+            }
+        });
+}
